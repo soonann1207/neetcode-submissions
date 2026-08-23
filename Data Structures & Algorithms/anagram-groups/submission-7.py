edc59690f84c4b1651f344b587908319ebf922cf
soonann1207 <83565527+soonann1207@users.noreply.github.com>
2026-08-23 14:31:40 +0800

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # if len(strs) == 0:
+        #     return [[""]]
+
+        hashmap = {}
+        for i in strs:
+            sorted_text = "".join(sorted(i))
+            if sorted_text not in hashmap:
+                hashmap[sorted_text] = []
+            hashmap[sorted_text].append(i)
+
+        return list(hashmap.values())
